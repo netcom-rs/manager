fb4355071ecce6febd9165642c690b1ee365fc79
Aleksa Markovic <aleksa.mm@gmail.com>
2023-10-04 23:58:24 +0200

    deploy-404

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router';
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.css";
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-import { FontAwesomeIcon } from './plugins/font-awesome'
-
-createApp(App).use(router).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
